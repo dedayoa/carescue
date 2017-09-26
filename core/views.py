e1999945d47711f116c27a1ffd92cd3c5b6d8fd2
Adedayo Ayeni <dedayoa@gmail.com>
2017-09-26 09:34:02 +0100

    wrong comparision fixed

--- core/views.py
+++ core/views.py
@@ -50,37 +50,38 @@ class ACUSSDCallback(views.View):
         #check if session already exists
         try:
             qs = QuerySession.objects.get(session_id=session_id)
-        
-            if text == "%s*%s*1"%(qs.loc_cood_x,qs.loc_cood_y):
+            
+            loc_x = qs.loc_cood_x*10000
+            loc_y = qs.loc_cood_y*10000
+            
+            if text == "%s*%s*1"%(loc_x, loc_y):
                 menu_text = "CON Mechanics near you... \n"
                 for mechanic in nearest_mechanics(session_id, 3):
                     m_obj = Mechanic.objects.get(id=mechanic[0])
                     menu_text += "%s, %s \n"%(m_obj.user.first_name, m_obj.phone_1)
                     menu_text += "Press 1 to Call a Mechanic, 0 to end.\n"
             
-            elif text == "%s*%s*2"%(qs.loc_cood_x,qs.loc_cood_y):
+            elif text == "%s*%s*2"%(loc_x, loc_y):
                 menu_text = "CON Towing Vehicles near you... \n"
                 for tow_vehicle in nearest_towing_vehicle(session_id, 3):
                     tv_obj = TowingVehicle.objects.get(id=tow_vehicle[0])
                     menu_text += "%s, %s \n"%(tv_obj.user.first_name, tv_obj.phone_1)
                     menu_text += "Press 1 to Call a Towing Vehicle, 0 to end.\n"
                     
-            elif text == "%s*%s*3"%(qs.loc_cood_x,qs.loc_cood_y):
+            elif text == "%s*%s*3"%(loc_x, loc_y):
                 menu_text = "CON Car parts near you... \n"
             
-            elif text == "%s*%s*4"%(qs.loc_cood_x,qs.loc_cood_y):
+            elif text == "%s*%s*4"%(loc_x, loc_y):
                 menu_text = "CON Cabs nearest you... \n"
                 
-            elif text == "%s*%s*1*1"%(qs.loc_cood_x,qs.loc_cood_y):
+            elif text == "%s*%s*1*1"%(loc_x, loc_y):
                 menu_text = "END Calling Mechanics"
                 
-            elif text == "%s*%s*2*1"%(qs.loc_cood_x,qs.loc_cood_y):
+            elif text == "%s*%s*2*1"%(loc_x, loc_y):
                 menu_text = "END Calling Towing Vehicle"
             
             elif text == "0":
                 menu_text = "END Goodbye"
-                
-            return HttpResponse(menu_text, content_type="text/plain", status=200)
         
         except QuerySession.DoesNotExist:
             #iterate through text and get coordinates
@@ -99,5 +100,5 @@ class ACUSSDCallback(views.View):
             menu_text += "3. Car Part \n"
             menu_text += "4. Cab"
             
-            return HttpResponse(menu_text, content_type="text/plain", status=200)
+        return HttpResponse(menu_text, content_type="text/plain", status=200)
         
